202baa1d8e6a6b42ba168f8ab1519943a3dc49c7
Joshua Mun <joshmun23@gmail.com>
2016-10-11 22:16:38 -0400

    birthdayCard

---
+++ client/main.js
@@ -0,0 +1,42 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  // this.counter = new ReactiveVar(0);
+  Session.setDefault('activeStep', 1);
+  Session.set('activeStep', 3);
+});
+
+Template.hello.helpers({
+  activeStep() {
+    return Session.get('activeStep');
+  },
+  step1() {
+    return Session.get('activeStep') === 1;
+  },
+  step2() {
+    return Session.get('activeStep') === 2;
+  },
+  step3() {
+    return Session.get('activeStep') === 3;
+  },
+});
+
+Template.hello.events({
+  'click button.continue'(event, instance) {
+    let currentStep = Session.get('activeStep');
+
+    Session.set('activeStep', currentStep + 1);
+    // $('.coupon').addClass('flip');
+
+    // setTimeout(function() {
+    //   // $('.coupon').removeClass('flip');
+    // }, 4000);
+  },
+  'click .img-thumb'(e, t) {
+    $(e.target).addClass('img');
+  }
+});
